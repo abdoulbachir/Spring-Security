b63e744f912cda2024fb450a431150fb222f1ec5
Abdoul Bachir <zabdoulbachir@gmail.com>
2024-04-20 17:41:27 -0500

    Added custom implementation of UserDetailsService

--- springsecSection3/src/main/java/com/eazybytes/EasyBankBackendApplication.java
+++ springsecSection3/src/main/java/com/eazybytes/EasyBankBackendApplication.java
@@ -2,8 +2,14 @@ package com.eazybytes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+//@EnableJpaRepositories("location of jpa repository")        If jpa interface is outside main package
+//@EntityScan("location of entity class")                     If entity class outside of main package
+//@EnableWebSecurity        Only need if building application using Spring without Spring boot
 public class EasyBankBackendApplication {
 
     public static void main(String[] args) {
--- springsecSection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ springsecSection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -3,13 +3,10 @@ package com.eazybytes.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -19,11 +16,13 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
          // Custom Security Configuration
+        // http.csrf(AbstractHttpConfigurer::disable);
         http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
-                .requestMatchers("/contact","/notices").permitAll());
+                .requestMatchers("/contact","/notices","/register","/error").permitAll());
         http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+        http.httpBasic(withDefaults()
+        ).csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
@@ -63,10 +62,10 @@ public class ProjectSecurityConfig {
         return new InMemoryUserDetailsManager(admin, user); // We can add as many user as we want in this constructor
     }*/
 
-    @Bean
-    public UserDetailsService userDetailsService (DataSource dataSource){
-        return new JdbcUserDetailsManager(dataSource);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService (DataSource dataSource){
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
