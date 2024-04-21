18c47e4a1f9ec55d18bf9112064f09bd9be65209
Abdoul Bachir <zabdoulbachir@gmail.com>
2024-04-21 17:59:32 -0500

    End of section 4. Added BCryptPasswordEncoder to our project

---
+++ springsecSection4/src/main/java/com/eazybytes/controller/LoginController.java
@@ -0,0 +1,43 @@
+package com.eazybytes.controller;
+
+import com.eazybytes.model.Customer;
+import com.eazybytes.repository.CustomerRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    CustomerRepository customerRepository;
+    PasswordEncoder passwordEncoder;
+
+    public LoginController(CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
+        this.customerRepository = customerRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
+        Customer savedCustomer = null;
+        ResponseEntity<String> response = null;
+        try {
+            String hashPwd = passwordEncoder.encode(customer.getPwd());
+            customer.setPwd(hashPwd);
+            savedCustomer = customerRepository.save(customer);
+            if (savedCustomer.getId() > 0) {
+                response = ResponseEntity
+                        .status(HttpStatus.CREATED)
+                        .body("User registered successfully");
+            }
+        }catch (Exception e) {
+            response = ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An exception has occurred due to " + e.getMessage());
+        }
+        return response;
+    }
+}
